d61870c7be4acb77651b159d34415958104aada2
Marion Regidor <marionpaularegidor@gmail.com>
2015-12-18 13:02:07 +0800

    Deleted ignored unit tests

--- src/test/java/com/orangeandbronze/enlistment/domain/SemesterEnlistmentTest.java
+++ src/test/java/com/orangeandbronze/enlistment/domain/SemesterEnlistmentTest.java
@@ -24,59 +24,6 @@ public class SemesterEnlistmentTest {
 		semesterEnlistment.enlistSection(section2);
 	}
 	
-	@Ignore
-	@Test(expected = PrerequisiteSubjectNotFoundException.class)
-	public void testNoPrerequsiteFoundInSemEnlist(){
-		Schedule schedule1 = new Schedule(Days.MTH, Period.H0830_1000);
-		Schedule schedule2 = new Schedule(Days.TF, Period.H0830_1000);
-		Room room = new Room("Room1", 5);
-		Semester semester1 = new Semester(2015, SemesterType.FIRST);
-		Section section1 = new Section("AAA111",semester1,schedule1,room,Subject.ENG202);
-		Section section2 = new Section("AAA111",semester1,schedule2,room,Subject.SCI101);
-		SemesterEnlistment semesterEnlistment = new SemesterEnlistment(semester1);
-		semesterEnlistment.enlistSection(section2);
-		semesterEnlistment.enlistSection(section1);
-	}
-	
-	@Ignore
-	@Test(expected = PrerequisiteSubjectNotFoundException.class)
-	public void testSuccessAddSectionToSemEnlist(){
-		Schedule schedule1 = new Schedule(Days.MTH, Period.H0830_1000);
-		Schedule schedule2 = new Schedule(Days.TF, Period.H0830_1000);
-		Room room = new Room("Room1", 5);
-		Semester semester1 = new Semester(2015, SemesterType.FIRST);
-		Section section1 = new Section("AAA111",semester1,schedule1,room,Subject.ENG202);
-		Section section2 = new Section("AAA222",semester1,schedule2,room,Subject.ENG101);
-		SemesterEnlistment semesterEnlistment = new SemesterEnlistment(semester1);
-		semesterEnlistment.enlistSection(section2);
-		semesterEnlistment.enlistSection(section1);
-		assertEquals(2, semesterEnlistment.getSections().size());
-	}
-	
-	@Ignore
-	@Test
-	public void testCollectPreviousSemesters(){
-		Semester semester1 = new Semester(2014, SemesterType.FIRST);
-		SemesterEnlistment semesterEnlistment1 = new SemesterEnlistment(semester1);
-		Schedule schedule1 = new Schedule(Days.MTH, Period.H0830_1000);
-		Schedule schedule2 = new Schedule(Days.TF, Period.H0830_1000);
-		Room room1 = new Room("Room1", 5);
-		Section section1 = new Section("AAA111",semester1,schedule1,room1,Subject.SCI101);
-		Section section2 = new Section("AAA222",semester1,schedule2,room1,Subject.ENG101);
-		semesterEnlistment1.enlistSection(section1);
-		semesterEnlistment1.enlistSection(section2);
-		
-		Semester semester2 = new Semester(2014, SemesterType.FIRST);
-		SemesterEnlistment semesterEnlistment2 = new SemesterEnlistment(semester1);
-		Schedule schedule3 = new Schedule(Days.MTH, Period.H0830_1000);
-		Schedule schedule4 = new Schedule(Days.TF, Period.H0830_1000);
-		Room room2 = new Room("Room1", 5);
-		Section section3 = new Section("BBB111",semester2,schedule3,room2,Subject.SCI101);
-		Section section4 = new Section("BBB222",semester2,schedule4,room2,Subject.ENG101);
-		semesterEnlistment2.enlistSection(section3);
-		semesterEnlistment2.enlistSection(section4);
-	}
-	
 	@Test
 	public void testCollectPrerequisiteSubject(){
 		Semester semester1 = new Semester(2014, SemesterType.FIRST);
